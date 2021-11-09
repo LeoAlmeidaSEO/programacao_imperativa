524476fe832c4f27110a00e63b9217419128d275
“Leonardo <leo.almeida.seo@gmail.com>
2021-11-09 20:46:10 -0300

    Checkpoint Programação Imperativa - Leo Almeida

---
+++ checkpoint-leo-almeida.js
@@ -0,0 +1,68 @@
+//Menu microondas 
+
+let menu = {
+    pipoca: 10,
+    macarrao: 8,
+    carne: 15,
+    feijao: 12,
+    brigadeiro: 8 
+};
+
+function menuMicroondas (segundos, prato) {
+    if (prato !== menu.feijao && menu.pipoca) {
+        return "Prato inexistente!";
+    } else if (segundos > 3*prato) {
+        return "Kabumm! ";
+    } else if (segundos > 2*prato) {
+        return "Comida queimou! :(";
+    } else if (segundos < prato) {
+        return "Tempo insuficiente!";
+    } else if (segundos == prato) {
+        return "Prato ponto, bom apetite!!! (tempo exato)"
+    } else {
+        return "Prato pronto, bom apetite!!! (passou do tempo):D";
+    }
+}
+
+//prencher campo 1: segundos 
+//prencher campo 2: menu.nomePrato ex: menu.feijao
+//pratos disponiveis (pipoca, macarrao, carne, feijao e brigadeiro)
+console.log(menuMicroondas(12,menu.feijao));
+
+
+//Funções de cada enunciado :)
+
+//function maior2x (segundos, prato) {
+//    if (segundos > 2*prato) {
+//        return "Comida queimou!";
+//    } else {
+//        return "Prato OK!";
+//    }
+//};
+
+//function tempoInsuficiente (segundos, prato) {
+//    if (segundos < prato) {
+//        return "Tempo insuficiente";
+//    } else {
+//        return "Prato OK!";
+//    }
+//}
+
+//function maior3x (segundos, prato) {
+//    if (segundos > 3*prato) {
+//        return "Kabumm!";
+//    } else {
+//        return "Prato OK!";
+//    }
+//};
+
+
+//function pratoPronto (prato) {
+//    if (prato !== menu.feijao && menu.pipoca) {
+//        return "Algo de errado não está certo";
+//    } else {
+//        return "Prato pronto!";
+//    }
+//}
+//
+//console.log(pratoPronto(menu.feijao));
